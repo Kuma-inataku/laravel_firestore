94d23c018958f12eb4ad714af4bb16f3c5d92605
takuya-inaguma <inataku0121@gmail.com>
2022-12-02 23:02:55 +0900

    first commmit

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirebaseController extends Controller
+{
+    public function index() {
+        return response()->json(['result' => true]);
+    }
+}
